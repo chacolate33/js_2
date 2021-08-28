4e227ce7728bf406764bbf3069a9575d1c631d2c
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-08-28 12:09:23 +0900

    [Finish] js_2

---
+++ app.js
@@ -0,0 +1,16 @@
+// let result = '';
+// let i = 0;
+
+// do {
+//   i += 1;
+//   result += i;
+// } while (i < 5);
+
+// alert(result);
+
+let i;
+let num = 0;
+for(i = 1;i < 11; i++) {
+  num += i;
+}
+alert(num);
